e5a934be8ea6a913731882752a723c32553b43f2
rafi-riday <rafi.riday@gmail.com>
2023-02-24 09:44:09 +0600

    Added Result system, implemented Search functionality in vocabulary. Project finished 🙃

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,29 @@
 <main class="m-4">
-    <div class="flex flex-col gap-2">
+    <div class="flex flex-col">
         <div>
-            <span class="text-[#ff3e00] text-lg pr-2">├</span>
+            <span class="text-[#ff3e00] text-xs sm:text-base pr-2">├</span>
             <a href="/test">
                 <button
-                    class="bg-[#ff3e0020] text-[#ff3e00] text-lg underline px-2 py-1 rounded"
+                    class="bg-[#ff3e0020] text-[#ff3e00] text-xs sm:text-base underline px-2 py-1 rounded my-1"
                     >Test
                 </button>
             </a>
         </div>
         <div>
-            <span class="text-[#ff3e00] text-lg pr-2">│ ├</span>
-            <a href="/test/math-vocabulary">
+            <span class="text-[#ff3e00] text-xs sm:text-base pr-2">│ ├</span>
+            <a href="/test/vocabulary">
                 <button
-                    class="bg-[#ff3e0020] text-[#ff3e00] text-lg underline px-2 py-1 rounded"
-                    >Math Vocabulary
+                    class="bg-[#ff3e0020] text-[#ff3e00] text-xs sm:text-base underline px-2 py-1 rounded my-1"
+                    >Vocabulary
                 </button>
             </a>
-            <span class="text-[#ff3e00] text-lg pr-2">├</span>
+            <span class="text-[#ff3e00] text-xs sm:text-base pr-2">├</span>
             <button
-                class="bg-[#ff3e0020] text-[#ff3e00] text-lg px-2 py-1 rounded cursor-default"
+                class="bg-[#ff3e0020] text-[#ff3e00] text-xs sm:text-base px-2 py-1 rounded my-1 cursor-default"
                 >Read vocabulary
             </button>
             <button
-                class="bg-[#ff3e0020] text-[#ff3e00] text-lg px-2 py-1 rounded cursor-default"
+                class="bg-[#ff3e0020] text-[#ff3e00] text-xs sm:text-base px-2 py-1 rounded my-1 cursor-default"
                 >Give test
             </button>
         </div>
